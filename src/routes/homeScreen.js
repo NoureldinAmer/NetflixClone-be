8d6b58dd3ca63cfb379404cc1e619d666ec64201
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2023-06-22 05:07:11 -0600

    add top rated shows to endpoint

--- src/routes/homeScreen.js
+++ src/routes/homeScreen.js
@@ -11,9 +11,11 @@ router.get("/", async (req, res) => {
   let endYear = currentYear;
   let startYear = currentYear - 5;
 
-  while (startYear >= 1975) {
+  while (startYear >= 1980) {
     const movies = await getTopRatedMovies(startYear, endYear);
+    const shows = await getTopRatedShows(startYear, endYear);
     results.push(movies);
+    results.push(shows);
     endYear = startYear - 1;
     startYear -= 5;
   }
@@ -53,6 +55,24 @@ async function getTopRatedMovies(startDate, endDate) {
     });
     return {
       list_name: `Top Rated Movies (${startDate} - ${endDate})`,
+      results: data.slice(0, 40),
+    };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+async function getTopRatedShows(startDate, endDate) {
+  try {
+    const response = await axios.get(
+      `${process.env.TMDB_BASE_URL}/discover/tv?api_key=${process.env.TMDB_API_KEY}&sort_by=vote_average.desc&vote_count.gte=600&primary_release_date.gte=${startDate}-01-01&primary_release_date.lte=${endDate}-12-31&region=US`
+    );
+    const data = response.data.results.map((result) => {
+      return { ...result, media_type: "tv" };
+    });
+    return {
+      list_name: `Top Rated Shows (${startDate} - ${endDate})`,
       results: data.slice(0, 40),
     };
   } catch (error) {
